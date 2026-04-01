9764080b358235dd022da895d515229aa8ca7874
mayurpandit25 <panditmayur888@gmail.com>
2026-04-01 13:33:08 +0530

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2" {
+/* resource "aws_instance" "ec2" {
    for_each = tomap({
     server1 = "t3.micro"
     server2 = "t3.small"
@@ -19,5 +19,15 @@ resource "aws_instance" "ec2" {
 
     tags = {
         Name = each.key
+    }
+}
+ */
+
+resource "aws_instance" "ec2" {
+    ami = "ami-05d2d839d4f73aafb"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "my-ec2"
     }
 }
--- provider.tf
+++ provider.tf
@@ -2,3 +2,15 @@ provider "aws" {
     region = "ap-south-1"
     profile = "config"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "mayur-s3-amazon-bucket75645"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+    profile = "config"
+    shared_credentials_files = [ "/root/.aws/credentials" ]
+    dynamodb_table = "cbz"
+  }
+}
+
