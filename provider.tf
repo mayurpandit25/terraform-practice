6e69415a6d35b7a145dea5e4c31872b8d7dcd811
mayurpandit25 <panditmayur888@gmail.com>
2026-04-01 11:15:34 +0530

    s3

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
     region = "ap-south-1"
-    profile = config
+    profile = "config"
 }
---
+++ s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "s3_bucket" {
+    region = "ap-south-1"
+    bucket = "mayur-s3-amazon-bucket54367"
+
+    tags   = {
+        Name = "mayur-s3-amazon-bucket54367"
+    }
+}
