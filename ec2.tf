dc96c9f1e6d3d7650e17e0606b0d1680d4394eca
mayurpandit25 <panditmayur888@gmail.com>
2026-04-01 13:23:16 +0530

    vpc

--- ec2.tf
+++ ec2.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "ec2" {
     ami = "ami-05d2d839d4f73aafb"
     instance_type = each.value
     key_name = "ubuntu"
-    vpc_security_group_ids = [ aws_security_group.sg.id ]
-    user_data = file("./user_data.sh")
+    #vpc_security_group_ids = [ aws_security_group.sg.id ]
+    user_data = file("/root/terraform-practice/user_data.sh")
     #count = 2
     depends_on = [ aws_security_group.sg ]
 
